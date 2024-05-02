93e51934559a8bfeb112d14c2834e966815fe276
Gururaj Mule <134521064+Inosukehashibirakingofthemountains@users.noreply.github.com>
2024-05-02 21:34:25 +0530

    initial commit

---
+++ src/components/Education/education.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+const Education = () => {
+  return (
+    <div>
+      <h2>Education</h2>
+      <ul>
+        <h3>Graducation</h3>
+        <li>Bachelor of Technology (B.Tech) in Electronic & Telecommunication.</li>
+        <li> Punyashlok Ahilyadevi Holkar Solapur Univercity, Solapur.
+        </li>
+        <li>Bachelor of Science in Computer Science, University of California, Berkeley</li>
+      </ul>
+    </div>
+  );
+};
+
+export default Education;
